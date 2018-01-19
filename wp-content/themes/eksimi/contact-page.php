8d9e4a577961002ecb18260c394f84ddcb8fa6b2
martyzzs <martins.fridenbergs93@gmail.com>
2018-01-19 16:34:55 +0200

    kontaktu lapa

--- wp-content/themes/eksimi/contact-page.php
+++ wp-content/themes/eksimi/contact-page.php
@@ -11,31 +11,42 @@
  */
 
 get_header();
-
+$pageID = get_the_ID();
 $posts = get_posts(array('category' => 29));
 
-masterslider(1);
+//masterslider(2);
+?>
 
+<div class="contact-background" style="background:url('<?php the_field('background',$pageID); ?>')">
+
+<?php
 if( have_posts() ):
-
     while( have_posts() ):  the_post(); ?>
-
-        <div class="title"> <?php the_title(); ?> </div>
-        <div> <?php the_content(); ?> </div>
-
 	    <?php
-
-	    if($posts){
-	    	foreach ($posts as $post)
-				the_field('name',$post->ID);
-	    }
-
+	    if($posts):
+	    	foreach ($posts as $post):
+	    	    ?>
+                <div class="contact-block-wrap">
+                    <div class="row contact-block">
+                        <div class="col-md-3 col-md-offset-1 contact-img" >
+                            <img src="<?php the_field('image',$post->ID); ?>">
+                        </div>
+                        <div class="col-md-5 contact-info">
+                            <div class="name"><?php the_field('name',$post->ID); ?></div>
+                            <div class="position"><?php the_field('position',$post->ID); ?></div>
+                            <div class="quote">"<?php the_field('quote',$post->ID); ?>"</div>
+                            <div class="email"><?php the_field('e-mail',$post->ID); ?></div>
+                        </div>
+                    </div>
+                </div>
+            <?php endforeach;
+	    endif;
 	    ?>
 
     <?php   endwhile;
-
 endif;
-
 ?>
+</div>
+<?php echo do_shortcode("[huge_it_maps id='1']"); ?>
 
 <?php get_footer(); ?>
--- wp-content/themes/eksimi/js/eksimi.js
+++ wp-content/themes/eksimi/js/eksimi.js
@@ -1,7 +1,17 @@
 (function ( $ ) {
-
+    //TODO deal with this when admin bar get biger on small screens
     $(window).resize(function(){
         $('.content-wrap').css('top',$('header').height()+20);
+
+        if($("#wpadminbar").length){
+            $('.content-wrap').css('top',$('header').height()+52);
+        }
+    });
+
+    $(document).ready(function(){
+        if($("#wpadminbar").length){
+            $('.content-wrap').css('top',$('header').height()+52);
+        }
     });
 
 })(jQuery);
